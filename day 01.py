1e62873549193249d896a428edeb088a5d8610a8
Ansh Sharma <136923285+ansh-2@users.noreply.github.com>
2023-11-03 13:51:56 +0530

    Day 01 file
    Today I learned about the basics of Python like Printing, Strings, Variable Naming, Input function

---
+++ day 01.py
@@ -0,0 +1,34 @@
+# print ("Python print function")
+# print("the function is declared like this;")
+# print("print('what to print')")
+
+# second code
+
+# print("hello " + input("what is your name?"))
+
+#third code
+
+# name = input("What is your name? ")
+# print(len(name))
+
+# fourth code
+# a = input("what is a: ")
+# b = input("what is b: ")
+
+# c = a
+# a = b
+# b = c
+
+# print ("a: " + a)
+  # print("b: " + b)(
+  
+  # fifth code
+
+#miniproject - name generator
+
+city_name = input("What is your city name?\n ")
+# print(city_name)
+fav_sport = input("Which sport did you like?\n ")
+# print(fav_sport)
+
+print("Name generated for you " + city_name + " " + fav_sport)
